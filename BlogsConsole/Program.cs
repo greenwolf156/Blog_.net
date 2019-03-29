76dbb9125ffddfec3667a7a67d25386d6151216c
greenwolf156 <greenwolf156@gmail.com>
2019-03-29 08:54:36 -0500

    Display blog count

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -14,53 +14,57 @@ namespace BlogsConsole
         public static void Main(string[] args)
         {
             logger.Info("Program started");
-            try
-            {
-                string choice;
-                string bln;
-                Console.WriteLine("1) Display all blogs");
-                Console.WriteLine("2) Add blog");
-                Console.WriteLine("3) Create Post");
-                Console.WriteLine("Enter to quit");
-                choice = Console.ReadLine();
-                logger.Info("User choice: {Choice}", choice);
-                var db = new BloggingContext();
-                if (choice == "1")
+            try {
+                string choice = "";
+                do
                 {
-                    var query = db.Blogs.OrderBy(b => b.Name);
-
-                    Console.WriteLine("All blogs in the database:");
-                    foreach (var item in query)
+                    string bln;
+                    Console.WriteLine("Enter Selection: ");
+                    Console.WriteLine("1) Display all blogs");
+                    Console.WriteLine("2) Add blog");
+                    Console.WriteLine("3) Create Post");
+                    Console.WriteLine("Enter q to quit");
+                    choice = Console.ReadLine();
+                    logger.Info("User choice: {Choice}", choice);
+                    var db = new BloggingContext();
+                    if (choice == "1")
                     {
-                        Console.WriteLine(item.Name);
+                        var query = db.Blogs.OrderBy(b => b.Name);
+                        int c = db.Blogs.Count();
+
+                        Console.WriteLine("{0} Blogs returned", c);
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                        Console.WriteLine();
                     }
-                }
-                else if (choice == "2")
-                {
-                    // Create and save a new Blog
-                    Console.Write("Enter a name for a new Blog: ");
-                    var name = Console.ReadLine();
+                    else if (choice == "2")
+                    {
+                        // Create and save a new Blog
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
 
-                    var blog = new Blog { Name = name };
+                        var blog = new Blog { Name = name };
 
-                    
-                    db.AddBlog(blog);
-                    logger.Info("Blog added - {name}", name);
-                }
-                else if (choice == "3")
-                {
-                    Console.Write("Enter blog name: ");
-                    bln = Console.ReadLine();
-                    Blog bl = db.RealBlog(bln);
-                    Console.Write("Enter Post Title: ");
-                    string blpt = Console.ReadLine();
-                    Console.Write("Enter Post Content: ");
-                    string blpc = Console.ReadLine();
-                    var post = new Post { Title = blpt, Content = blpc, Blog = bl};
-                    db.MakePost(post);
-                    logger.Info("Post made");
-                } while (choice == "1" || choice == "2"|| choice == "3") ;
 
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", name);
+                    }
+                    else if (choice == "3")
+                    {
+                        Console.Write("Enter blog name: ");
+                        bln = Console.ReadLine();
+                        Blog bl = db.RealBlog(bln);
+                        Console.Write("Enter Post Title: ");
+                        string blpt = Console.ReadLine();
+                        Console.Write("Enter Post Content: ");
+                        string blpc = Console.ReadLine();
+                        var post = new Post { Title = blpt, Content = blpc, Blog = bl };
+                        db.MakePost(post);
+                        logger.Info("Post made");
+                    }
+                } while ((choice != "q" || choice != "Q") && (choice == "1" || choice == "2" || choice == "3"));
             }
             catch (Exception ex)
             {
